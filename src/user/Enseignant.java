36cbe1d5c1c16ed7a15418b00b6e0e9c66422e12
Da-HaTer <oussamah4boubi@gmail.com>
2022-04-29 18:38:30 +0100

    add and delete queries for enseignant

--- src/user/Enseignant.java
+++ src/user/Enseignant.java
@@ -1,43 +1,123 @@
 package user;
 
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.PreparedStatement;
+
 public class Enseignant extends Utilisateur{
-	private String name;
-	private String cin;
-	private String id;
+	private int ide;
+	private String prenom;
+	private String nom;
 	
 	public String getName() {
-		return name;
+		return nom;
 	}
 	public void setName(String name) {
-		this.name = name;
+		this.nom = name;
 	}
 	public String getCin() {
-		return cin;
+		return prenom;
 	}
 	public void setCin(String cin) {
-		this.cin = cin;
+		this.prenom = cin;
 	}
-	public String getId() {
-		return id;
+	public int getId() {
+		return ide;
 	}
-	public void setId(String id) {
-		this.id = id;
+	public void setId(int id) {
+		this.ide = id;
 	}
 	public Enseignant(String login, String passwd) {
 		super(login,passwd);
 		// TODO Auto-generated constructor stub
 	}
-	public Enseignant(String id, String cin, String name) { //only id cin name (superadmin)
+	public Enseignant(int id, String prenom, String nom) { //only id cin name (superadmin)
 		super();
-		this.id = id;
-		this.cin = cin;
-		this.name = name;
+		this.ide = id;
+		this.prenom = prenom;
+		this.nom = nom;
 	}
-	public Enseignant(String id, String cin, String name, String login, String pwd) {
-		super(login,pwd);
-		this.id = id;
-		this.cin = cin;
-		this.name = name;
+	public Enseignant(int id,String login, String pwd, int ide, String prenom , String nom ) {
+		super(id,login,pwd,ide,2);
+		this.ide = ide;
+		this.prenom = prenom;
+		this.nom = nom;
 		// TODO Auto-generated constructor stub
 	}
+	
+	public void save_ens_db() {
+		this.save_user_DB();
+        int exist=fetch_enseignant(this.ide);
+        if (exist==0) {
+			try{ 
+	            String query = "insert into enseignant values (?,?,?);"; // WHERE Login=? and Pwd=?";
+	            java.sql.Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_test?characterEncoding=utf8","root","toor");
+	            PreparedStatement preparedStmt = (PreparedStatement) connection.prepareStatement(query);
+	            
+	            preparedStmt.setInt(1, ide); preparedStmt.setString(2, nom); preparedStmt.setString(3, prenom);
+				
+	            int rowsaffected = preparedStmt.executeUpdate();
+	            System.out.println(rowsaffected);
+	
+	            connection.close();
+	        }
+	        catch (SQLException e) {e.printStackTrace();}
+        }
+	}
+	
+	private void delete_db(int id) {
+		// TODO Auto-generated method stub
+		
+        try{
+        	String query1 = "select Login from user where idRef=? and userType=2;";
+        	String query2 = "delete from enseignant where idEnseignant=?;"; // 
+        	
+            java.sql.Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_test?characterEncoding=utf8","root","toor");
+            PreparedStatement preparedStmt = (PreparedStatement) connection.prepareStatement(query1);
+            preparedStmt.setInt(1, ide);
+            ResultSet r = preparedStmt.executeQuery();
+            while (r.next()) {
+            	String id_user= r.getString(1);
+            	DBdelete_by_login(id_user);
+            }
+        	
+            PreparedStatement preparedStmt1 = (PreparedStatement) connection.prepareStatement(query2);
+            
+            preparedStmt1.setInt(1, ide);
+            int rowsaffected = preparedStmt1.executeUpdate();
+            System.out.println(rowsaffected);
+            
+            connection.close();
+        } 
+        
+        catch (SQLException e) {e.printStackTrace();}
+	}
+	
+    public int fetch_enseignant(int id) {
+        try{ 
+            String query = "select count(*) from enseignant where idEnseignant=?;"; // WHERE Login=? and Pwd=?";
+            java.sql.Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_test?characterEncoding=utf8","root","toor");
+            PreparedStatement preparedStmt = (PreparedStatement) connection.prepareStatement(query);
+            
+            preparedStmt.setInt(1, id);
+            
+            int res=0;
+            ResultSet r = preparedStmt.executeQuery();
+            r.next();
+            res= r.getInt(1);
+            connection.close();
+            return res;
+        }
+        catch (SQLException e) {e.printStackTrace();}
+        return -1;
+    }
+	
+	public static void main(String[] args) {
+		Enseignant ens=new Enseignant(3,"khalil","raboudi",2,"khalil","gargouri");
+//		ens.save_ens_db();
+		ens.delete_db(2);
+	}
+	
 }
--- src/user/Super_Admin.java
+++ src/user/Super_Admin.java
@@ -1,6 +1,6 @@
 package user;
 
-public class Super_Admin extends Utilisateur{
+public class Super_Admin extends Utilisateur{ ///to be deleted : admin has proprety superadmin
 	public Super_Admin(String login, String passwd) {
 		super(login,passwd);
 		// TODO Auto-generated constructor stub
